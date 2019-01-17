100cc7ad476ca352e67e2bb82f0e8093684bbe43
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2019-01-17 12:16:50 -0500

    refactor

--- theme.js
+++ theme.js
@@ -1,12 +1,14 @@
-import theme from 'mdx-deck/themes'
+// import theme from 'mdx-deck/themes'
+import { yellow as theme } from 'mdx-deck/themes'
 
 export default {
   ...theme,
-
+  fontSizes: [
+    '0.5em', '0.75em', '1em', '1.5em', '2em', '3em'
+  ],
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
 }
